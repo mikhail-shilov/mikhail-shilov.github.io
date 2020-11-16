cdf5f3f4e95d0a5eac0a232a8db3bbb082a87dc0
Mikhail Shilov <59969482+mikhail-shilov@users.noreply.github.com>
2020-11-16 21:42:22 +0300

    Upload SPA build

---
+++ precache-manifest.0e36a8721584967e36723a46118af195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f750b57c469974cb9c2658ebabdc95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceff9b5ea4bcf5df4a6d",
+    "url": "/static/css/main.8d8865ed.chunk.css"
+  },
+  {
+    "revision": "af3a79be819f46f6dcb6",
+    "url": "/static/js/2.d27b4f97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d27b4f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceff9b5ea4bcf5df4a6d",
+    "url": "/static/js/main.4b11ea7c.chunk.js"
+  },
+  {
+    "revision": "b3270964a844516928d6",
+    "url": "/static/js/runtime-main.bef3e883.js"
+  }
+]);
