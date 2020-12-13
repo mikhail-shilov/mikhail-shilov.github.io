d6f7382ce8b9a40e117e5c03082c9d62b6f345dd
Mikhail Shilov <mih.shilov@gmail.com>
2020-12-13 22:20:37 +0300

    'update'

---
+++ precache-manifest.d4cffd32b60b88f36d56000b2034a23f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417f1cd807b228da7cf1c1532ec255a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27e27ed6ce6ea2ec5aea",
+    "url": "/static/css/main.584c6888.chunk.css"
+  },
+  {
+    "revision": "c6136262ff555d5a077d",
+    "url": "/static/js/2.443d2953.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.443d2953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e27ed6ce6ea2ec5aea",
+    "url": "/static/js/main.2480296a.chunk.js"
+  },
+  {
+    "revision": "b3270964a844516928d6",
+    "url": "/static/js/runtime-main.bef3e883.js"
+  }
+]);
