ff9b89867945ad1d6cceef190dd198805640fa33
Mikhail Shilov <mih.shilov@gmail.com>
2021-01-03 21:35:55 +0300

    'update'

---
+++ precache-manifest.ebf31f172349034b86bf5ab15bad6955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ed9ec252bc0c5d44edefe19c034aad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1deb5c9d17b706950169",
+    "url": "/static/css/main.e7874137.chunk.css"
+  },
+  {
+    "revision": "f80cd3142a0a6c170228",
+    "url": "/static/js/2.acf03db1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.acf03db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1deb5c9d17b706950169",
+    "url": "/static/js/main.360e2031.chunk.js"
+  },
+  {
+    "revision": "b3270964a844516928d6",
+    "url": "/static/js/runtime-main.bef3e883.js"
+  }
+]);
